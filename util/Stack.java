c9e073f29f53d2fafdc278bda7d58b2beeb9c930
letimome <leticia.montalvillo@ehu.es>
2015-04-21 15:29:26 +0200

    Update Stack.java

--- util/Stack.java
+++ util/Stack.java
@@ -1,11 +1,12 @@
 package util;
 
 public class Stack {
+	
+	
 private int maxCount = 100;
 public void push(Object obj) {
 	if(count==maxCount)
-	  return
-	else
-original(obj);
+	  return;
+	else original(obj);
 }
 }
